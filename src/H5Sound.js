b4343ba60e66c5c1f792101c2de09a055d15c569
cup <changewater@qq.com>
2015-10-20 16:36:44 +0800

    registerPlugins
    createjs.Sound.registerPlugins([createjs.WebAudioPlugin]);

--- src/H5Sound.js
+++ src/H5Sound.js
@@ -1,6 +1,6 @@
 var H5Sound= {
     load : function(sounds, fun) {
-
+        createjs.Sound.registerPlugins([createjs.WebAudioPlugin]);
         createjs.Sound.alternateExtensions = ["mp3"];   // add other extensions to try loading if the src file extension is not supported
         createjs.Sound.addEventListener("fileload", createjs.proxy(soundLoaded, this)); // add an event listener for when load is completed
         createjs.Sound.registerSounds(sounds);
